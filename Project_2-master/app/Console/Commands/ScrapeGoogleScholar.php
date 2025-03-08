addf3e28e06b2813aef6044aff00662a40a8e2ca
=Aekarattakhat <=aekarattakhat.s@kkumail.com>
2025-03-09 01:02:32 +0700

    full filled all func in highlighteditor

--- Project_2-master/app/Console/Commands/ScrapeGoogleScholar.php
+++ Project_2-master/app/Console/Commands/ScrapeGoogleScholar.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Services\GetHighlight;
+use App\Services\HighlightEditor;
 
 class ScrapeGoogleScholar extends Command
 {
@@ -32,12 +32,26 @@ class ScrapeGoogleScholar extends Command
      */
     public function handle()
     {
+        $news_item = [
+            "news_id"=> 1,
+            "banner" => "https://computing.kku.ac.th/images/news/2025-03-04-sartra-banner.jpg",
+            "tags" => [
+                2
+            ],
+            "publish_status" => "highlight",
+            "publish" => "2025-03-04",
+            "latest_update" => "2025-03-04",
+            "title" => "",
+            "content" => "<p>วันที่ <strong>7 กุมภาพันธ์ 2568</strong></p>dddddddddd",
+            "editor_author" => 151
+        ];
         $scholarId = $this->argument('scholar_id');
-        //$data = GetHighlight::getAllNews();
-        //$data = GetHighlight::getNews(1);
-        //$data = GetHighlight::getHighlights();
-        //$data = GetHighlight::getNewsbyTag(2);
-        $data = GetHighlight::getTags();
+        //$data = HighlightEditor::createNews($news_item,151);
+        //$data = HighlightEditor::deleteNews(17);
+        //$data = HighlightEditor::updateNewsContent(16,$news_item);
+        //$data = HighlightEditor::updateNewsStatus(16,"not_published");
+        //$data = HighlightEditor::createTag("cpx");
+        $data = HighlightEditor::deleteTag(3);
         echo json_encode($data);
         return 0;
 
