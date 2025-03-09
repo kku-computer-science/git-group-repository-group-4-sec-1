b644889e1b7c18d83b079348d077a8463156000c
Poompiput Malimatr <poompiput.m@kkumail.com>
2025-03-09 23:52:11 +0700

    html file

---
+++ temp/preview_high.blade.php
@@ -0,0 +1,34 @@
+ @extends('dashboards.users.layouts.user-dash-layout')
+<link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.3/css/fixedHeader.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.12.0/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.3/css/fixedHeader.bootstrap4.min.css">
+@section('content')
+
+<div class="container">
+    {{-- <form action="{{ route('generate_pdf') }}" method="GET"> --}}
+        <input type="hidden" name="id" value="{{Auth::id()}}"> <!-- ใช้ ID ผู้ใช้ที่ล็อกอิน -->
+       
+        {{-- tag --}}
+        {{-- เผยแพร่ --}}
+        {{-- เนื้อหา --}}
+
+
+
+        {{-- <button type="submit" class="btn btn-primary">Preview PDF</button> --}}
+        <button type="submit" name="download" value="1" class="btn btn-danger ">แก้ไข </button>
+        <button type="submit" name="download" value="1" class="btn btn-danger ">เผยแพร่ </button>
+        {{-- <a href="{{ route('generate_word', ['id' => Auth::user()->id]) }}" class="btn btn-primary mx-3">Download as Word</a>  --}}
+    </form>
+
+    <div id="pdf-preview" class="mt-4">
+        <iframe src="{{ route('generate_pdf') }}" width="100%" height="600px"></iframe>
+    </div>
+    
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src = "http://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js" defer ></script>
+<script src = "https://cdn.datatables.net/1.12.0/js/dataTables.bootstrap4.min.js" defer ></script>
+<script src = "https://cdn.datatables.net/fixedheader/3.2.3/js/dataTables.fixedHeader.min.js" defer ></script>
+
+@stop 
