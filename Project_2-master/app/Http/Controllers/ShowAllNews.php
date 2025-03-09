0b989b4cc8102511358e730af84f3fb5b90a6ce5
waristongnam <waris.t@kkumail.com>
2025-03-09 20:51:01 +0700

    update controller

--- Project_2-master/app/Http/Controllers/ShowAllNews.php
+++ Project_2-master/app/Http/Controllers/ShowAllNews.php
@@ -13,17 +13,30 @@ class ShowAllNews extends Controller{
 
     public function index(Request $request){
         $News = GetHighlight::getAllNews();
+
+        $Tag = Tag::All();
         
         $News_Pub = $News->filter(function ($item) {
             return $item['publish_status'] !== 'not_published';
         });
 
+        //ค้นหาด้วย Search
         if ($request->has('search') && !empty($request->search)){
             $search = strtolower($request->search);
 
             $News_Pub = $News_Pub->filter(function ($item) use ($search){
                 return str_contains(strtolower($item['title']),$search) ||
                         str_contains(strtolower($item['content']),$search);                
+            });
+        }
+
+        if ($request->has('tag') && !empty($request->tag)){
+            $selected = $request->tag;
+
+            $newsWithTagIds = NewsTag::where('tag_id',$selected)->pluck('news_id');
+
+            $News_Pub = $News_Pub->filter(function ($item) use ($newsWithTagIds){
+                return in_array($item['news_id'],$newsWithTagIds->toArray());
             });
         }
 
@@ -34,7 +47,7 @@ class ShowAllNews extends Controller{
         $publishNews = $publishNews->sortByDesc('publish');
 
         $SortNews = $highlightNews->merge($publishNews);        
-        
+        dd($SortNews);
         return view('highlight', ['SortNews' => $SortNews]);
     }
 
