75f5cefaa13b475ce7dad037a52f758979204f79
Nattapat1234 <nattapat.ph@kkumail.com>
2025-03-10 22:42:34 +0700

    create test sprint3

--- Project_2-master/routes/web.php
+++ Project_2-master/routes/web.php
@@ -101,7 +101,8 @@ Route::get('bib/{id}', [BibtexController::class, 'getbib'])->name('bibtex');
 Route::get('/highlight', [ShowAllNews::class, 'index'])->name('highlight.index');
 
 //สำหรับหน้ารายละเอียดข่าว
-Route::get('/news/{id}',[ReadNewsController::class,'index'])->name('news.details');
+Route::get('/news/{id}',[ReadNewsController::class,'index'])->name('news.index');
+
 
 
 //Route::get('bib/{id}', [BibtexController::class, 'index'])->name('bibtex');
